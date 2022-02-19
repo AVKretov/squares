baf5be8b3b9e3f01d402e11d31b9594fab943293
Aleksandr Kretov <kretov_a@outlook.com>
2022-02-19 21:36:10 +0300

    Reformat

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -10,7 +10,7 @@ class SQRServiceTest {
     void squaresInRange() {
         SQRService service = new SQRService();
 
-        int range1 =200;
+        int range1 = 200;
         int range2 = 300;
         long expected = 3;
 
@@ -19,11 +19,12 @@ class SQRServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
     @Test
     void squaresLowRangeTestL() {
         SQRService service = new SQRService();
 
-        int range1 =99;
+        int range1 = 99;
         int range2 = 100_000_000;
         long expected = 90;
 
@@ -32,11 +33,12 @@ class SQRServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
     @Test
     void squaresLowRangeTestM() {
         SQRService service = new SQRService();
 
-        int range1 =100;
+        int range1 = 100;
         int range2 = 100_000_000;
         long expected = 90;
 
@@ -45,11 +47,12 @@ class SQRServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
     @Test
     void squaresLowRangeTestH() {
         SQRService service = new SQRService();
 
-        int range1 =101;
+        int range1 = 101;
         int range2 = 100_000_000;
         long expected = 89;
 
@@ -58,11 +61,12 @@ class SQRServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
     @Test
     void squaresHighRangeTestL() {
         SQRService service = new SQRService();
 
-        int range1 =100;
+        int range1 = 100;
         int range2 = 9800;
         long expected = 89;
 
@@ -71,11 +75,12 @@ class SQRServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
     @Test
     void squaresHighRangeTestN() {
         SQRService service = new SQRService();
 
-        int range1 =100;
+        int range1 = 100;
         int range2 = 9801;
         long expected = 90;
 
@@ -84,11 +89,12 @@ class SQRServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
     @Test
     void squaresHighRangeTestH() {
         SQRService service = new SQRService();
 
-        int range1 =100;
+        int range1 = 100;
         int range2 = 9802;
         long expected = 90;
 
